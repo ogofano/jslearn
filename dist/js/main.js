bd6e79b684517711900ee4ca1cb145f55cf2ad9f
Ilya <mailboxilya65@gmail.com>
2023-03-28 19:22:08 +0300

    Add new functions from the second practice video

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms, lastFilm, markFilm;
+
+do {
+  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+} while (numberOfFilms < 0 || !numberOfFilms || numberOfFilms === "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,8 +14,27 @@ const personalMovieDB = {
   private: false
 };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?");
-let markFilm = +prompt("Во сколько оцениваешь его?");
-personalMovieDB.movies[lastFilm] = markFilm;
+for (let i = 0; i < 2; i++) {
+
+  do {
+    lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+  } while (!lastFilm || lastFilm === "" || lastFilm == 0 || lastFilm.length > 50);
+
+  do {
+    markFilm = prompt("Во сколько оцениваешь его?", "");
+  } while (!markFilm || markFilm === "" || markFilm > 10 || markFilm < 0);
+  
+  personalMovieDB.movies[lastFilm] = markFilm;
+};
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы - классический зритель!");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  throw Error;
+}
 
 console.log(personalMovieDB);
